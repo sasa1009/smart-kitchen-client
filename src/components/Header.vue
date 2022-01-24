5ac119724a3d2c9ee874a56f14d00020c9c830e4
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2022-01-25 01:30:13 +0900

    fortawesome関連のパッケージを追加 未ログイン状態のヘッダーを追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const input = ref('');
+</script>
+
 <template>
   <div class="header-content">
     <el-button
@@ -11,7 +16,7 @@
         type="text"
         class="menu-item"
       >
-        ユーザー登録
+        ログイン
       </el-button>
       <el-button
         type="text"
@@ -23,8 +28,23 @@
         type="text"
         class="menu-item"
       >
-        ログイン
+        ユーザー登録
       </el-button>
+      <el-button
+        type="text"
+        class="menu-item"
+      >
+        レシピ一覧
+      </el-button>
+      <el-button
+        class="search-button"
+        round
+      >
+        検索
+      </el-button>
+      <div class="search-bar">
+        <el-input v-model="input" placeholder="レシピ名・食材で検索" />
+      </div>
     </div>
   </div>
 </template>
@@ -42,14 +62,30 @@
   color: black;
   font-size: 22px;
 }
+.search-bar {
+  float: right;
+  width: 230px;
+}
+.search-button {
+  float: right;
+  color: black;
+  font-size: 16px;
+  height: 100%;
+}
+.el-button {
+  margin-left: 0;
+}
 .menu {
   float: right;
   height: 40px;
+  width: 750px;
   margin-top: 10px;
 }
 .menu-item {
+  float: right;
   color: black;
-  font-size: 18px;
+  font-size: 16px;
   height: 100%;
+  margin-left: 10px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import './element-variables.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far)
 
 const app = createApp(App)
-
 app
   .use(router)
   .use(ElementPlus)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
