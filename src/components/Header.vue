2a76f01f5eaea6d9ba56840225acd5955bcf7f9e
sasa1009 <35237920+sasa1009@users.noreply.github.com>
2022-01-25 17:52:58 +0900

    未ログイン状態のヘッダーにレスポンシブ機能を追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { useMq } from "vue3-mq";
 const input = ref('');
 const mq = useMq();
+const isDrawerOpen = ref(false);
 </script>
 
 <template>
@@ -14,7 +16,10 @@ const mq = useMq();
     >
       SMART KITCHEN
     </el-button>
-    <div class="search">
+    <div
+      v-if="mq.current !== 'sm'"
+      class="search"
+    >
       <el-input
         class="search-bar"
         v-model="input"
@@ -27,7 +32,10 @@ const mq = useMq();
         検索
       </el-button>
     </div>
-    <div class="menu">
+    <div
+      v-if="mq.current === 'lg'"
+      class="menu-lg"
+    >
       <el-button
         type="text"
         class="menu-item"
@@ -53,6 +61,72 @@ const mq = useMq();
         レシピ一覧
       </el-button>
     </div>
+    <div
+      v-else
+      class="menu-md"
+    >
+      <el-popover
+        placement="bottom-start"
+        :width="200"
+        trigger="click"
+      >
+        <template #reference>
+          <el-button
+            type="text"
+            class="menu-icon"
+          >
+            <font-awesome-icon
+              :icon="['fas', 'bars']"
+            />
+          </el-button>
+        </template>
+        <el-menu text-color="#000000">
+          <el-menu-item index="1">
+            <span>レシピ一覧</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <span>ユーザー登録</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <span>ゲストログイン</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <span>ログイン</span>
+          </el-menu-item>
+        </el-menu>
+      </el-popover>
+      <el-button
+        v-if="mq.current === 'sm'"
+        @click="isDrawerOpen = true"
+        type="text"
+        class="menu-icon"
+      >
+        <font-awesome-icon
+          :icon="['fas', 'search']"
+        />
+      </el-button>
+      <el-drawer
+        v-model="isDrawerOpen"
+        direction="ttb"
+        size="20%"
+      >
+        <div
+          class="search"
+        >
+          <el-input
+            class="search-bar"
+            v-model="input"
+            placeholder="レシピ名・食材で検索"
+          />
+          <el-button
+            class="search-button"
+            round
+          >
+            検索
+          </el-button>
+        </div>
+      </el-drawer>
+    </div>
   </div>
 </template>
 
@@ -61,6 +135,7 @@ const mq = useMq();
   height: 60px;
   max-width: 1200px;
   margin: 0 auto;
+  padding: 0 5px;
   vertical-align: middle;
 }
 .logo {
@@ -85,10 +160,16 @@ const mq = useMq();
   height: 100%;
   margin-left: 5px;
 }
-.menu {
+.menu-lg {
   float: right;
   height: 40px;
   width: 400px;
+  margin-top: 10px;
+}
+.menu-md {
+  float: right;
+  height: 40px;
+  width: 100px;
   margin-top: 10px;
 }
 .menu-item {
@@ -97,5 +178,22 @@ const mq = useMq();
   font-size: 16px;
   height: 100%;
   margin-left: 10px;
+}
+.menu-icon {
+  float: right;
+  color: black;
+  font-size: 30px;
+  padding: 0;
+  margin-left: 15px;
+}
+.popover-menu {
+  text-align: center;
+}
+.popover-item {
+  color: black;
+  font-size: 16px;
+}
+.el-menu {
+  border-right: none;
 }
 </style>
